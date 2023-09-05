6d8e46e69e18c2af8fa04086d91c2b49ebc6abee
malheiros <dmalheiros77@gmail.com>
2023-09-05 03:35:29 -0300

    pokemon module update

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { PokemonService } from './pokemon.service';
 
 @Controller('pokemon')
 export class PokemonController {
+    constructor(private pokemonService: PokemonService) {}
 
     @Get()
     async getAll() {
-
+        return await this.pokemonService.getAll();
     }
 
     @Get(':id_or_name')
@@ -15,7 +17,7 @@ export class PokemonController {
 
     @Get('/type/:type')
     async getByType(@Param('type') type: string) {
-
+        
     }
 
 }   
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,6 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { DBService } from '../database/db.service';
 
 @Injectable()
 export class PokemonService {
-    
+    constructor(
+        private readonly dbService: DBService
+    ) { }
+
+    async getAll(){
+        return await this.dbService.getAll();;
+    }
 }
